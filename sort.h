22ca620ab2a60a42bdb3a6b238b8bc437d58c2b3
Brian-Wagura <briankiraguri@gmail.com>
2024-03-04 12:44:50 +0300

    Counting sort

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define _SORT_H_
 
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,5 +27,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* _SORT_H_ */
